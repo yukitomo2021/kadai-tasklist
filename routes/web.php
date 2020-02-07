4df575d94005cb842a4c9b286c9285c7909e98c3
yukitomo2021 <kashiwabara2021@gmail.com>
2020-02-07 02:34:56 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,32 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks','TasksController');
+
+
+/*
+//CRUD
+//タスクの個別詳細ページ表示
+Route::get('tasks/{id}', 'TasksController@show');
+
+//タスクの新規登録を処理
+Route::post('tasks', 'TasksController@store');
+
+//タスクの更新処理
+Route::put('tasks/{id}', 'TasksController@update');
+
+//タスクを削除
+Route::delete('tasks/{id}', 'TasksController@destroy');
+
+
+//index:showの補助ページ
+Route::get('tasks','TasksController@index')->name('tasks.index');
+
+//create:新規作成用のフォームページ
+Route::get('tasks/create','TasksController@create')->name('tasks.create');
+
+//edit:更新用のフォームページ
+Route::get('tasks/{id}/edit','TasksController@edit')->name('tasks.edit');
+*/
