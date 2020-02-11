e226cf11b9b094c3a1fca67453e61f86f826e60f
yukitomo2021 <kashiwabara2021@gmail.com>
2020-02-11 02:42:07 +0000

    crud tasklist

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,17 +2,31 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        <title>TaskList</title>
+        <title>MessageBoard</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     </head>
 
     <body>
+        <header class="mb-4">
+            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+                <a class="navbar-brand" href="/">MessageBoard</a>
+                
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="nav-bar">
+                    <ul class="navbar-nav mr-auto"></ul>
+                    <ul class="navbar-nav">
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        
         @include('commons.navbar')
         
         <div class="container">
-            @include('commons.error_messages')
-            
             @yield('content')
         </div>
         
